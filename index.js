2d2a1d7150bac7e1a3c8fc20609371b5394f907b
handsomemolly <ryan.kendig@gmail.com>
2021-01-11 20:49:22 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arr, word) {
+    let cards = [];
+    for (let i = 0; i < arr.length; i++) {
+        cards.push(`Thank you, ${arr[i]}, for the wonderful ${word} gift!`)
+    }
+    return cards;
+}
+
+
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num);
+}
